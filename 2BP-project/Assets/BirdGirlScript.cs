9bb3672542472ffac0ad2f609122430cc14d4d92
LucasKayon <lucas.pc92@gmail.com>
2024-08-17 16:31:06 -0300

    Jump Limit Implementation

--- 2BP-project/Assets/BirdGirlScript.cs
+++ 2BP-project/Assets/BirdGirlScript.cs
@@ -1,35 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static CharacterController2D;
+using UnityEngine.Events;
 
 public class BirdGirlScript : MonoBehaviour
 {
+    [SerializeField] private LayerMask m_WhatIsGround;                          // A mask determining what is ground to the character
+    [SerializeField] private Transform m_GroundCheck;                           // A position marking where to check if the player is grounded.
+    [SerializeField] private Transform m_CeilingCheck;							// A position marking where to check for ceilings
+
     public Rigidbody2D myRigidBody;
     public float flapIntensity;
     public Animator animator;
+
+    const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+    private bool m_Grounded;            // Whether or not the player is grounded.    
+    public UnityEvent OnLandEvent;
 
     //[SerializeField] private Animator anim;
     //[SerializeField] private float attackSpeed;
     //[SerializeField] private float damage;
 
     float timeUntilAttack;
+    int currentEnergy;
+    public LogicScript logic;
+    public int maxEnergy = 2;
 
     // Start is called before the first frame update
     void Start()
     {
-        //timeUntilAttack = 0f; // Initialize timeUntilAttack to 0 to allow immediate attack
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+
+        currentEnergy = maxEnergy;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z) == true)
+
+
+        if (Input.GetKeyDown(KeyCode.Z) == true && logic.energy >=1)
         {
+            logic.decreaseEnergy(1);
             myRigidBody.velocity = Vector2.up * flapIntensity;            
             animator.Play("Bird_Girl_Fly");
             //animator.SetBool("IsFlying", true);
         }
 
         
+    }
+
+    private void Awake()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+
+        if (OnLandEvent == null)
+            OnLandEvent = new UnityEvent();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        bool wasGrounded = m_Grounded;
+        m_Grounded = false;
+
+        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {                
+                m_Grounded = true;
+                
+                if (!wasGrounded)
+                { 
+                    
+                    OnLandEvent.Invoke();
+                    logic.setEnergyValue(2);
+                }
+                    
+            }
+        }
     }
 }
--- 2BP-project/Assets/LogicScript.cs
+++ 2BP-project/Assets/LogicScript.cs
@@ -6,19 +6,54 @@ using UnityEngine.UI;
 public class LogicScript : MonoBehaviour
 {
     public int hp;
+    public int energy;
     public Text hpIndicator;
+    public Text energyIndicator;
 
     void Start()
     {
         hp = 3;
+        energy = 2;
     }
     
 
-    [ContextMenu("Decrease HP")]
+   [ContextMenu("Decrease HP")]
    public void detractHp(int scoreToDetract)
     {
         hp = hp - scoreToDetract;
         //Debug.Log($"HP decreased, current HP: {hp}");
         hpIndicator.text = $"HP: {hp}";
+    }
+
+    [ContextMenu("Increase Energy")]
+    public void increaseEnergy(int energyToIncrease)
+    {
+        energy = energy + energyToIncrease;
+        //Debug.Log($"ENERGY increased, current ENERGY: {energy}");
+        energyIndicator.text = $"ENERGY: {energy}";
+    }
+
+    [ContextMenu("Decrease Energy")]
+    public void decreaseEnergy(int energyToDecrease)
+    {
+        energy = energy - energyToDecrease;
+        //Debug.Log($"ENERGY increased, current ENERGY: {energy}");
+        energyIndicator.text = $"ENERGY: {energy}";
+    }
+
+    [ContextMenu("Set Energy Value")]
+    public void setEnergyValue(int energyValue)
+    {
+        energy = energyValue;
+        Debug.Log($"ENERGY RESET, current ENERGY: {energy}");
+        energyIndicator.text = $"ENERGY: {energy}";
+    }
+
+    void Update()
+    {
+        if (hp <= 0)
+        {
+
+        }
     }
 }
